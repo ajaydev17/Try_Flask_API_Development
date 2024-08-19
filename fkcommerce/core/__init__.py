7ffb13d5f57e253eaf43a7855def22ccb29441e2
Ajay Dev <devadigaajay1729@gmail.com>
2024-08-19 22:49:57 +0530

    Added the first route

--- fkcommerce/core/__init__.py
+++ fkcommerce/core/__init__.py
@@ -20,6 +20,7 @@ def create_app(config_type=os.getenv('CONFIG_TYPE')):
     app = Flask(__name__)
     app.config.from_object(config_type)
     initialize_extension(app)
+    register_blueprint(app)
     return app
 
 
@@ -28,3 +29,8 @@ def initialize_extension(app):
     db_migrate.init_app(app, database)
 
     import core.models  # noqa: F401
+
+
+def register_blueprint(app):
+    import core.inventory_api
+    app.register_blueprint(core.inventory_api.inventory_category_api_blueprint)
