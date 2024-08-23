2b422ff37e4a23b58dd9a555d5ef567e12219ffd
Ajay Dev <devadigaajay1729@gmail.com>
2024-08-24 00:47:11 +0530

    Added different schema for GET and POST method for Category

--- fkcommerce/core/inventory_api/routes.py
+++ fkcommerce/core/inventory_api/routes.py
@@ -1,22 +1,23 @@
 from . import inventory_category_api_blueprint
-from ..schema import CategorySchema
+from ..schema import CategoryResponseSchema, CategoryInsertSchema
 from apifairy import response, body
 from ..models import Category
 from .. import database
 
-category_schema = CategorySchema(many=True)
+category_response_schema = CategoryResponseSchema(many=True)
+category_insert_schema = CategoryInsertSchema()
 
 
 @inventory_category_api_blueprint.route('/category', methods=['GET'])
-@response(category_schema)
+@response(category_response_schema)
 def category():
     return Category.query.all()
 
 
 @inventory_category_api_blueprint.route('/category', methods=['POST'])
-@body(category_schema)
+@body(category_insert_schema)
 def insert_category(kwargs):
     new_category = Category(**kwargs)
     database.session.add(new_category)
     database.session.commit()
-    return
+    return new_category
--- fkcommerce/core/schema.py
+++ fkcommerce/core/schema.py
@@ -1,9 +1,14 @@
 from . import marshmallow
 
 
-class CategorySchema(marshmallow.Schema):
+class CategoryResponseSchema(marshmallow.Schema):
     id = marshmallow.Integer(dump_only=True)
     name = marshmallow.String(required=True)
     slug = marshmallow.String(required=True)
+
+
+class CategoryInsertSchema(marshmallow.Schema):
+    name = marshmallow.String(required=True)
+    slug = marshmallow.String(required=True)
     is_active = marshmallow.Boolean(required=True)
-    parent_id = marshmallow.Integer()
+    parent_id = marshmallow.Integer(allow_none=True)
